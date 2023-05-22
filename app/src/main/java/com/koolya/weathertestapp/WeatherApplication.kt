4e41e4fdd82103d533366c709b03c8fc6a093d8b
illiakulikov <illyakul1996@gmail.com>
2023-05-22 17:12:19 +0300

    added dependencies in app

--- app/src/main/java/com/koolya/weathertestapp/WeatherApplication.kt
+++ app/src/main/java/com/koolya/weathertestapp/WeatherApplication.kt
@@ -1,6 +1,12 @@
 package com.koolya.weathertestapp
 
 import android.app.Application
+import com.google.android.gms.location.LocationServices
+import com.koolya.weathertestapp.data.di.ApiModule
+import com.koolya.weathertestapp.data.di.DataModule
+import com.koolya.weathertestapp.data.di.DispatchersModule
+import com.koolya.weathertestapp.domain.di.DomainModule
+import com.koolya.weathertestapp.ui.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,7 +19,11 @@ class WeatherApplication : Application() {
             androidLogger()
             androidContext(this@WeatherApplication)
             modules(
-
+                ApiModule,
+                PresentationModule,
+                DispatchersModule,
+                DataModule,
+                DomainModule,
             )
         }
     }
